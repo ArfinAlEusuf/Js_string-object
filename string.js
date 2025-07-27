6adc1a01e4edb58fc39e78052973cf232d45de77
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-28 02:42:09 +0600

    strring

---
+++ string.js
@@ -0,0 +1,19 @@
+const country = 'Bangaldesh';
+const division = "Newkhale";
+const district = `B-Bariya`;
+// const thana = new String('Demra')
+
+// console.log(thana);
+
+const numbers = [54, 98, 78, 21, 65];
+// console.log(numbers.lenght);
+// console.log(numbers[1])
+
+// string is immutable ---> can't change
+// array is mutable ---> you van change the elaments
+
+const capital= 'Dhaka';
+// console.log(capital.length);
+// console.log(capital[0]);
+console.log(capital);
+capital[0]= "F"
