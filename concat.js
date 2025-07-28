8c9154ac160fde21e3bc18884cf808d5e0ce8694
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-28 20:08:10 +0600

    concat

---
+++ concat.js
@@ -0,0 +1,9 @@
+const frist = 'abid';
+const last = "nabid";
+
+// const fullName= frist + last;
+const fullName= frist +' '+ last;
+// console.log(fullName);
+
+const fullName2 = frist.concat(' ').concat(last)
+console.log(fullName2);
