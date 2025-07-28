f3827550af705639a3e090df66840303c49b33ba
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-28 19:49:19 +0600

    case trim

---
+++ case.js
@@ -0,0 +1,24 @@
+const school = 'RAJUK  urrora model school'
+
+console.log(school.toUpperCase());
+console.log(school.toLocaleLowerCase());
+
+const subject = 'Chemistry';
+const book = 'chemistry';
+
+if (subject === book){
+    console.log('iam reading a book eibar potikkahy fatay felbo');
+}
+else{
+    console.log('hudi pata ultai')
+}
+
+const drink = 'water';
+const liquid = 'water ';
+
+if(drink.trim()=== liquid.trim()){
+    console.log('pani er opor nam jibon');
+}
+else{
+    console.log('somuder pani ache khaite pari na')
+}
