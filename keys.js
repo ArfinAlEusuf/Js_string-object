c55fbf0d00e7ac172be4baa3ee2a8dee76dfd8de
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-29 00:36:32 +0600

    keys

---
+++ keys.js
@@ -0,0 +1,10 @@
+const computer ={
+    brand: 'lenovo',
+    price: 50000,
+    processor: 'intel',
+    hdd: '512GB',
+    monitor: 'hp',
+}
+
+const keys = Object.keys(computer);
+console.log(keys)
