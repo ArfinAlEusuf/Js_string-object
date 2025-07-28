61b737d6691ec5c63f562f68fb1d499c65ce4bb9
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-29 00:40:11 +0600

    values

---
+++ values.js
@@ -0,0 +1,10 @@
+const computer ={
+    brand: 'lenovo',
+    price: 50000,
+    processor: 'intel',
+    hdd: '512GB',
+    monitor: 'hp',
+}
+
+const values = Object.values(computer);
+console.log(values);
