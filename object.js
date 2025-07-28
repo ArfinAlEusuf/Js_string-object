b179e23cc93f1750a0755e7d4654503f74be43b4
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-28 20:58:31 +0600

    object

---
+++ object.js
@@ -0,0 +1,32 @@
+// primitive
+
+const age = 21;
+const school = 'rifle square';
+const isPassed = true;
+// let isDevloper;
+
+const subjects = ['bangla', 'english', 'physics', 'math']
+const bottle_wrong = ['white', 45]
+const bottleColor = 'white';
+const bottlePrize = 45;
+const bottleDrinks = 'water';
+
+// object
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+}
+
+// non-primitive
+const subject = {
+    name: 'biology',
+    teacher: 'rasheda mem',
+    examDate: '30 feb',
+    chaptar: ['first','second','third'],
+    exam:{
+        name: 'Final Exam',
+        marks: 100
+    }
+}
