39d5ba326723d1dabc2c9a6e56d2f3e92979d651
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-28 20:00:05 +0600

    slice

---
+++ slice.js
@@ -0,0 +1,10 @@
+const address= 'anderKilla';
+const part = address.slice(2,5);
+console.log(part);
+
+const sentence = ' i am a good boy and hardworking person.'
+// console.log(sentence.split(' '));
+console.log(sentence.split('a'));
+
+const friends = 'rahim,kahim,dahi,lahim,fahim,sahim'
+console.log(friends.split(','));
