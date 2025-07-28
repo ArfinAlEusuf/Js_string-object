9196f1e55ef3d4516f5554620d918980c0bbd360
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-29 00:07:46 +0600

    dot notation

---
+++ property.js
@@ -0,0 +1,14 @@
+const person = {
+    name: 'sodor uddin',
+    age: '25',
+    profession: 'developer',
+    salary: 25000,
+    married: true
+}
+
+// console.log(person)
+
+// dot notation
+console.log(person.profession)
+const income= person.salary
+console.log(income)
