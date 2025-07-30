11d02d4bd41ffb3eea214dd616326ecac5fffee2
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-07-30 20:28:00 +0600

    task 2 done

--- task.js
+++ task.js
@@ -1,13 +1,43 @@
-const str = ["happy ending", 'the end']; 
-const targetLetter = "n";
-let count = 0;
-let char = str[1];
-console.log(char);
-for (let i = 0; i < str.length; i++) {
-    if (str[i] === targetLetter) {
-        count++;
-    }
-}
+// const str = ["happy ending", 'the end']; 
+// const targetLetter = "n";
+// let count = 0;
+// let char = str[1];
+// // console.log(char);
+// for (let i = 0; i < str.length; i++) {
+//     if (str[i] === targetLetter) {
+//         count++;
+//     }
+// }
 
 // console.log(`${targetLetter} found ${count} times in ${str}`)
 // console.log('n is:', count)
+
+
+// task 1 
+const nam= 'ononno';
+const target = 'o';
+
+let total= 0;
+for(i = 0; i < nam.length; i++){
+    if(nam[i] === target){
+        total=total+1;
+    }
+}
+// console.log(total);
+
+// task 2 
+
+const cheakName = 'Amaar Nam Arfin';
+const target1= 'a';
+const target2= 'A';
+
+let totalCount= 0 ;
+for(i=0; i< cheakName.length; i++){
+    if (cheakName[i]=== target1){
+        totalCount++;
+    }
+    else if (cheakName[i]=== target2){
+        totalCount++;
+    }
+}
+console.log(`ei name e total A ba a ache ${totalCount} bar`)
